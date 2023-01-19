a6a5228c6903361d56969ef698fe9f8a572477fb
Yago <yagos-s@live.com>
2023-01-18 21:11:32 -0300

    Criando uma classe no php

---
+++ PHP-POO/Filme.php
@@ -0,0 +1,43 @@
+<?php
+
+class Filme
+{
+    private $titulo;
+    private $diretores;
+    private $atores;
+    private $personagens;
+    private $produtoras;
+    private $lancamento;
+    private $nota;
+    private $duração;
+    private $enredo;
+
+    public function __construct(string $titulo,
+                                string $diretores,
+                                string $atores,
+                                string $personagens,
+                                string $produtoras,
+                                string $lancamento,
+                                float $nota,
+                                int $duração,
+                                string $enredo)
+        {
+
+            $this->titulo = $titulo;
+            $this->diretores = $diretores;
+            $this->atores = $atores;
+            $this->personagens = $personagens;
+            $this->produtoras = $produtoras;
+            $this->lancamento = $lancamento;
+            $this->nota = $nota;
+            $this->duração = $duração;
+            $this->enredo = $enredo;
+        }
+
+    public function dicaFilme()
+    {
+        echo $this->titulo . PHP_EOL;
+        echo $this->diretores;
+    }
+}
+
---
+++ PHP-POO/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'Filme.php';
+
+$avatar = new Filme(
+    'Avatar',
+    'Yago',
+    'Vitoria',
+    'Maria Alice',
+    'Marvel',
+    '22/12/2022',
+    8.2,
+    240,
+    'Os caras azuis nadando'
+);
+
+$avatar->dicaFilme();
+
